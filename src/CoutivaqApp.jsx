e9d0ee783d7f990109bf1b6bb5f0aec259666399
Sebastian Rodriguez <sebaser99@gmail.com>
2022-10-10 22:55:08 -0500

    primeros pasos

---
+++ src/CoutivaqApp.jsx
@@ -0,0 +1,14 @@
+import { Box, Container } from "@mui/material";
+import { Navbar } from "./components/Navbar";
+
+
+export const  CoutivaqApp = () => {
+
+  return (
+    <Container maxWidth='lg'>
+      <Navbar />
+    </Container>
+  )
+}
+
+
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { CoutivaqApp } from './CoutivaqApp'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CoutivaqApp/>
+  </React.StrictMode>
+)
